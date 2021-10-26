42fd4a18415771c0dae65a2bec72654200e35711
tamuna-momo <tamuna-momo@users.noreply.github.com>
2021-10-26 10:20:48 +0400

    make changes in js code

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 // 1.
-const numberArray=[17,20,54,67,71];
+const numberArray=[17, 20, 54, 67, 71];
 
-const sum=numberArray.reduce(function(a,b){
+const sum=numberArray.reduce(function(a, b){
     return a+b}, +0);
 
 console.log(sum);
 
 // 2.
 
-const personArray = [{name: "natia", age: 29, address:"mukhiani"}, {name: "milana", age: 29, address:"gldani"}, {name: "tamuna", age: 30, address:'vazisubani'}];
+const personArray = [
+       {name: "natia",
+        age: 29,
+         address:"mukhiani"
+       }, 
+       {name: "milana",
+        age: 29,
+         address:"gldani"
+       },
+       {name: "tamuna",
+        age: 30,
+         address:'vazisubani'
+       }
+];
 
 // 3.
 console.log('my name is', (personArray[0].name));
@@ -16,11 +29,11 @@ console.log('my age is',(personArray[0].age));
 console.log('my address is',(personArray[0].address));
 
 // 4.
-
  if ((personArray[0].age) < 19) {
         console.log('i am a teenager');
  } else {
         console.log("i am an adult");
 } 
+
 
 
